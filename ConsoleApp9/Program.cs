a16668a89592fa7c4dcc25c07c956fc27527201b
XuJiaHui12321 <1951294228@qq.com>
2021-08-17 22:49:49 +0800

    添加项目文件。

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ServiceStack.Redis;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRedisClient redisClient = new RedisClient("106.14.249.170",6379,"3edcVFRS",0);
+            var s = redisClient.GetHashKeys("0054514f-2e4d-47ff-84b7-32c5355e73c4");
+            string s1 = "123";
+            Console.WriteLine(s1);
+        }
+    }
+}
